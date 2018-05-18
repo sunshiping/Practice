cf628df7385f94016732fd37b7e11255669dd77e
sunshiping <sunshiping1117@163.com>
2018-05-18 11:34:28 +0800

    练习

--- demo/App.js
+++ demo/App.js
@@ -1,58 +1,40 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React,{Component}from 'react';
+import {View,Text,StyleSheet} from 'react-native';
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
+export default class HelloWorldApp extends Component {
+  render(){
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Text style={styles.red}>Hello world</Text>
       </View>
-    );
+    )
   }
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+  container:{
+    backgroundColor:'#6435c9',
+    flex:1,
+    margin:30px,
+    borderWidth:1,
+    borderColor:'#6435c9',
+    borderRadius:16,
+    shadowColor:'#6435c9',
+    shadowOpacity:0.6,
+    shadowRadius:2,
+    shadowOffset:{
+      height:1,
+      width:0
+    }
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  red:{
+    fontSize:20,
+    textAlign:'center',
+    fontStyle:'italic',
+    letterSpacing:2,
+    lineHeight:33,
+    fontWeight:'900',
+    textDecorationLine:'underline',
+    textDecorationStyle:'dashed',
+    color:'red'
+  }
+})
