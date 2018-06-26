fb4f27b2c8ea1cc1e06b1674705bd48e31edb241
sunshiping <sunshiping1117@163.com>
2018-06-26 11:03:44 +0800

    react-native

--- 房信/pages/index/index.js
+++ 房信/pages/index/index.js
@@ -1,3 +1,4 @@
+
 Page({
   data: {
     background: ['http://7xsuhd.com1.z0.glb.clouddn.com/house_146770749014.jpg', 'http://7xsuhd.com1.z0.glb.clouddn.com/house_146770749014.jpg', 'http://7xsuhd.com1.z0.glb.clouddn.com/house_146770749014.jpg'],
@@ -10,8 +11,7 @@ Page({
     previousMargin: 0,
     nextMargin: 0,
     num: 1,
-    grids: [
-      {
+    grids: [{
         name: "二手房",
         url: '/pages/list/list?type=house&id=1'
       },
@@ -30,100 +30,100 @@ Page({
     ]
   },
   /**
-     * 生命周期函数--监听页面加载
-     */
-  onLoad: function (options) {
-
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-   
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
     console.log('666')
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
 
 
-  changeProperty: function (e) {
+  changeProperty: function(e) {
     var propertyName = e.currentTarget.dataset.propertyName
     var newData = {}
     newData[propertyName] = e.detail.value
     this.setData(newData)
   },
-  changeIndicatorDots: function (e) {
+  changeIndicatorDots: function(e) {
     this.setData({
       indicatorDots: !this.data.indicatorDots
     })
   },
-  changeAutoplay: function (e) {
+  changeAutoplay: function(e) {
     this.setData({
       autoplay: !this.data.autoplay
     })
   },
-  intervalChange: function (e) {
+  intervalChange: function(e) {
     this.setData({
       interval: e.detail.value
     })
   },
-  durationChange: function (e) {
+  durationChange: function(e) {
     this.setData({
       duration: e.detail.value
     })
   },
-  changeType: function (e) {
+  changeType: function(e) {
     console.log(e);
     this.setData({
       num: e.target.dataset.num
     })
-    if(this.data.num == 1){
+    if (this.data.num == 1) {
       console.log('二手房')
-    }else{
+    } else {
       console.log('租房')
     }
   },
-  onShareAppMessage: function (res) {
+  onShareAppMessage: function(res) {
     if (res.from === 'button') {
       // 来自页面内转发按钮
       console.log(res.target)
