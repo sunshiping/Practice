a5fc8cc972152505fa100cdbbf63b5c2eed28aa3
孙世平 <sunshiping1117@163.com>
2018-09-20 14:19:18 +0800

    获取profile数据

--- nodeDemo/config/keys.js
+++ nodeDemo/config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoURI:"mongodb://test:s123456@ds135441.mlab.com:35441/restful-api-prod",
+  // mongoURI:"mongodb://test:s123456@ds135441.mlab.com:35441/restful-api-prod",
+  mongoURI:"mongodb://test:a123456@ds163842.mlab.com:63842/node-vue-element",
   secretOrKey:"secret"
 }
--- nodeDemo/routes/api/profiles.js
+++ nodeDemo/routes/api/profiles.js
@@ -12,5 +12,36 @@ router.get('/test',(req,res) => {
   res.json({msg:'Profile works'});
 });
 
+// 添加
+// @route POST api/profiles/add
+router.post('/add',passport.authenticate("jwt",{session:false}),(req, res) => {
+  const profileFileds = {};
+
+  if(req.body.type)profileFileds.type = req.body.type;
+  if(req.body.describe)profileFileds.describe = req.body.describe;
+  if(req.body.income)profileFileds.income = req.body.income;
+  if(req.body.expend)profileFileds.expend = req.body.expend;
+  if(req.body.cash)profileFileds.cash = req.body.cash;
+  if(req.body.remark)profileFileds.remark = req.body.remark;
+
+  new Profile(profileFileds).save().then(profile => {
+    res.json(profile);
+  });
+});
+
+
+// 获取
+// @route POST api/profiles
+router.get('/',passport.authenticate("jwt",{session:false}),(req, res) => {
+  Profile.find()
+  .then(profile => {
+    if(!profile){
+      return res.status(404).json("没有任何数据");
+    }
+    res.json(profile);
+  })
+  .catch(err => res.status(404).json(err));
+});
+
 
 module.exports = router;
