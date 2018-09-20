d36967decf20a6122d5542d78f255bca717cd093
孙世平 <sunshiping1117@163.com>
2018-09-20 19:04:22 +0800

    注册、404

--- nodeDemo/client/src/router.js
+++ nodeDemo/client/src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from './views/Index.vue'
+import Register from './views/Register'
+import NotFound from './views/404'
 
 Vue.use(Router)
 
@@ -16,6 +18,16 @@ export default new Router({
       path: '/index',
       name: 'index',
       component: Index
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    },
+    {
+      path: '*',
+      name: '/404',
+      component: NotFound
     }
     // {
     //   path: '/about',
@@ -26,4 +38,4 @@ export default new Router({
     //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     // }
   ]
-})
+});
