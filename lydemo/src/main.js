0a4776e8a353f96ef711c2eb4001da1cf9115c61
孙世平 <sunshiping1117@163.com>
2018-10-17 13:06:17 +0800

    提交main.js

---
+++ lydemo/src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import VueI18n from 'vue-i18n'
+
+const FastClick = require('fastclick')
+FastClick.attach(document.body)
+
+Vue.config.productionTip = false
+Vue.use(VueI18n)
+const i18n = new VueI18n({
+  locale: 'zh-CN',
+})
+new Vue({
+  i18n,
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
