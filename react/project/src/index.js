5e31ffd1f1fdcf55129d9c1549981591667373d5
孙世平 <sunshiping1117@163.com>
2018-12-07 11:53:21 +0800

    react-redux

--- react/project/src/index.js
+++ react/project/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoList from './TodoList';
-
-ReactDOM.render(<TodoList />, document.getElementById('root'));
+import TodoList1 from './TodoList1';
+import {Provider} from 'react-redux';
+import store from './store'
+const App = (
+  <Provider store={store}>
+    <TodoList1 />
+  </Provider>
+)
+ReactDOM.render(App, document.getElementById('root'));
 
--- react/project/src/store/index.js
+++ react/project/src/store/index.js
@@ -1,15 +1,10 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './reducer';
 import thunk from 'redux-thunk';
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
 const enhancer = composeEnhancers(
   applyMiddleware(thunk)
 );
 const store = createStore(reducer, enhancer);
-// const store = createStore(
-//   reducer,
-//   applyMiddleware(thunk)
-//    );
 
 export default store;
